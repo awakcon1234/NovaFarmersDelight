cfc760d85ffa969e637625ce9f4ceaf47f8e690b
Katalijst <quentin.bedarride@gmail.com>
2024-10-24 22:46:50 +0200

    Init project

--- src/main/kotlin/fr/ateastudio/farmersdelight/registry/Items.kt
+++ src/main/kotlin/fr/ateastudio/farmersdelight/registry/Items.kt
@@ -5,10 +5,6 @@ import net.kyori.adventure.text.Component
 import xyz.xenondevs.nova.addon.registry.ItemRegistry
 import xyz.xenondevs.nova.initialize.Init
 import xyz.xenondevs.nova.initialize.InitStage
-import xyz.xenondevs.nova.resources.builder.model.ModelBuilder
-import xyz.xenondevs.nova.resources.builder.task.model.ItemModelContent
-import xyz.xenondevs.nova.resources.layout.block.BlockModelSelectorScope
-import xyz.xenondevs.nova.resources.layout.item.ItemModelSelectorScope
 
 @Suppress("unused")
 @Init(stage = InitStage.PRE_PACK)
