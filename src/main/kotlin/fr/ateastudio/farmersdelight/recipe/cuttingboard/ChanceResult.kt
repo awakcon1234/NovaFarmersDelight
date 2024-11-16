7d2d20a54e3ab16c6b5829d01cc98be3dbbc71ce
Katalijst <quentin.bedarride@gmail.com>
2024-11-16 18:49:14 +0100

    Cutting board recipe logic done

--- src/main/kotlin/fr/ateastudio/farmersdelight/recipe/cuttingboard/ChanceResult.kt
+++ src/main/kotlin/fr/ateastudio/farmersdelight/recipe/cuttingboard/ChanceResult.kt
@@ -1,10 +1,6 @@
 package fr.ateastudio.farmersdelight.recipe.cuttingboard
 
-import com.google.gson.JsonElement
-import com.google.gson.JsonSyntaxException
-import net.minecraft.util.GsonHelper
 import org.bukkit.inventory.ItemStack
-import xyz.xenondevs.nova.util.item.ItemUtils.getItemStack
 import kotlin.random.Random
 
 const val CUTTING_BOARD_FORTUNE_BONUS = 0.5
@@ -24,20 +20,5 @@ class ChanceResult
         val out: ItemStack = stack.clone()
         out.amount = outputAmount
         return out
-    }
-    
-    companion object {
-        
-        fun deserialize(je: JsonElement): ChanceResult {
-            if (!je.isJsonObject) throw JsonSyntaxException("Must be a json object")
-            
-            val json = je.asJsonObject
-            val itemId = GsonHelper.getAsString(json, "item")
-            val count = GsonHelper.getAsInt(json, "count", 1)
-            val chance = GsonHelper.getAsFloat(json, "chance", 1f)
-            val itemStack: ItemStack = getItemStack(itemId).asQuantity(count)
-            
-            return ChanceResult(itemStack, chance)
-        }
     }
 }
