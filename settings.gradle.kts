6da0950cb4a162d65162a2e4f6abc8822e3420e9
Katalijst <quentin.bedarride@gmail.com>
2025-03-07 10:49:47 +0100

    Updated to Nova 0.18-RC.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("xyz.xenondevs.nova:catalog:0.18-RC.1")
+            from("xyz.xenondevs.nova:catalog:0.18-RC.2")
         }
     }
 }
