fb032264576b37fbc3c3d36b2bd2408ff29de369
awakcon1234 <awakcon1234@gmail.com>
2025-12-05 09:27:15 +0700

    bump catalog to `0.21.0` and add github action build workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("xyz.xenondevs.nova:catalog:0.21.0-alpha.4")
+            from("xyz.xenondevs.nova:catalog:0.21.0")
         }
     }
 }
